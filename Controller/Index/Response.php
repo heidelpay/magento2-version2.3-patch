6a0742efe12989710ffc4a6f4095c61e224b9072
Simon Gabriel <simon_gabriel@gmx.de>
2018-12-20 15:22:20 +0100

    [change] (MAGE2-106) Add fix to enable Response Controller for Magento 2.3. This is no backwards compatible solution!!!

--- Controller/Index/Response.php
+++ Controller/Index/Response.php
@@ -8,6 +8,9 @@ use Heidelpay\Gateway\Model\ResourceModel\PaymentInformation\CollectionFactory;
 use Heidelpay\Gateway\Model\TransactionFactory;
 use Heidelpay\PhpPaymentApi\Exceptions\HashVerificationException;
 use Heidelpay\PhpPaymentApi\Response as HeidelpayResponse;
+use Magento\Framework\App\CsrfAwareActionInterface;
+use Magento\Framework\App\Request\InvalidRequestException;
+use Magento\Framework\App\RequestInterface;
 use Magento\Quote\Model\Quote;
 use Magento\Quote\Model\QuoteRepository;
 use Magento\Sales\Model\Order;
@@ -35,7 +38,7 @@ use Magento\Sales\Model\Order\Email\Sender\OrderSender;
  *
  * @package heidelpay\magento2\controllers
  */
-class Response extends \Heidelpay\Gateway\Controller\HgwAbstract
+class Response extends \Heidelpay\Gateway\Controller\HgwAbstract implements CsrfAwareActionInterface
 {
     /** @var QuoteRepository */
     private $quoteRepository;
@@ -298,5 +301,31 @@ class Response extends \Heidelpay\Gateway\Controller\HgwAbstract
         // return the heidelpay response url as raw response instead of echoing it out.
         $result->setContents($redirectUrl);
         return $result;
+    }
+
+    /**
+     * Create exception in case CSRF validation failed.
+     * Return null if default exception will suffice.
+     *
+     * @param RequestInterface $request
+     *
+     * @return InvalidRequestException|null
+     */
+    public function createCsrfValidationException(RequestInterface $request): ?InvalidRequestException
+    {
+        return null;
+    }
+
+    /**
+     * Perform custom request validation.
+     * Return null if default validation is needed.
+     *
+     * @param RequestInterface $request
+     *
+     * @return bool|null
+     */
+    public function validateForCsrf(RequestInterface $request): ?bool
+    {
+        return true;
     }
 }
